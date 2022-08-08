25f4b594f3717ac23d4b74750b28bc9cea2d6e71
Joao <joaop86@hotmail.com>
2022-08-08 01:18:18 +0100

    fix select value

--- planning/src/components/SettingsForm/index.tsx
+++ planning/src/components/SettingsForm/index.tsx
@@ -23,8 +23,14 @@ const SettingsForm = () => {
         className="border-b-2 focus:border-purple-300 focus:outline-none focus:shadow-outline"
       />
 
-      <select className="block" name="sequence" id="sequence">
-        <option value="0">Select card values</option>
+      <select
+        value={formik.values.sequence}
+        onChange={formik.handleChange}
+        className="block"
+        name="sequence"
+        id="sequence"
+      >
+        <option value="">Select card values</option>
         <option value="1">Fibonacci</option>
         <option value="2">Sequential</option>
       </select>
