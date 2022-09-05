ffa182abb9887e09f5bb6d5f68a44f6289bc2de9
Joao <joaop86@hotmail.com>
2022-09-05 01:35:09 +0100

    selected state

--- planning/src/components/PokerCard/index.tsx
+++ planning/src/components/PokerCard/index.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 type PokerCardProps = {
   value: number;
 };
 
 const PokerCard = ({ value }: PokerCardProps) => {
+  const [isSelected, setIsSelected] = useState(false);
+
   return (
-    <div className="p-6 border border-gray-500 rounded cursor-pointer shadow-xl motion-safe:hover:scale-105 text-center">
-      <span className="text-purple-300 font-bold text-2xl">{value}</span>
+    <div
+      className={`p-6 border border-gray-500 rounded cursor-pointer shadow-xl motion-safe:hover:scale-105 text-center ${
+        isSelected ? "bg-purple-100 scale-105" : ""
+      }`}
+      onClick={() => setIsSelected((prev) => !prev)}
+    >
+      <span
+        className={`text-purple-${
+          isSelected ? "500" : "300"
+        } font-bold text-2xl`}
+      >
+        {value}
+      </span>
     </div>
   );
 };
