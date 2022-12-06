ca8206fafd2e6de3677c68351c90327e49cc3b78
Joao <joaop86@hotmail.com>
2022-12-06 17:01:14 +0000

    some fixes

--- planning/src/components/TopNavbar/index.tsx
+++ planning/src/components/TopNavbar/index.tsx
@@ -2,18 +2,18 @@ import Button from "@components/Button";
 import { faBook, faCode } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { highlighted } from "../../styles/globals.css";
-import { container, process } from "./styles.css";
+import { buttons, container, process, text } from "./styles.css";
 
 const TopNavbar = () => {
   return (
     <div className={container}>
       <Link href="/">
-        <span className="font-bold p-2 text-gray-700">
+        <span className={text}>
           Trust the <span className={`${highlighted} ${process}`}>Process</span>
         </span>
       </Link>
-      <div className="ml-auto mr-2 text-gray-700">
-        <a href="https://github.com/demo-hub/AgilePlanning">
+      <div className={buttons}>
+        <a href="https://github.com/demo-hub/TrustTheProcess">
           <Button type="no-fill" label="Check the code" icon={faCode} />
         </a>
         <a href="https://liberapay.com/sombra/donate">
--- planning/src/components/TopNavbar/styles.css.ts
+++ planning/src/components/TopNavbar/styles.css.ts
@@ -13,3 +13,14 @@ export const process = style({
   fontStyle: "italic",
   letterSpacing: "0.05em",
 });
+
+export const text = style({
+  padding: "0.5rem",
+  color: "#374151",
+  fontWeight: 700,
+});
+
+export const buttons = style({
+  marginRight: "0.5rem",
+  color: "#374151",
+});
